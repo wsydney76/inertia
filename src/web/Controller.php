a8df285c3ac4735099821fa6d41965ec64a5c3dc
wsydn <wsydney@web.de>
2022-06-27 17:34:37 +0200

    Initial Commit

---
+++ src/web/Controller.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace wsydney76\inertia\web;
+
+use Craft;
+use wsydney76\inertia\Inertia;
+
+/**
+ *
+ * @property-read string $inertiaVersion
+ * @property-read string $inertiaUrl
+ */
+class Controller extends \craft\web\Controller
+{
+
+    public array|int|bool $allowAnonymous = true;
+
+    private ?string $only = '';
+
+    public function beforeAction($action): bool
+    {
+        if (Craft::$app->request->headers->has('X-Inertia-Partial-Data')) {
+            $this->only = Craft::$app->request->headers->get('X-Inertia-Partial-Data');
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $component
+     * @param array $params
+     * @return array|string
+     */
+    public function inertia(string $component, array $params = []): array|string
+    {
+        $params = [
+            'component' => $component,
+            'props' => $this->getInertiaProps($params),
+            'url' => $this->getInertiaUrl(),
+            'version' => $this->getInertiaVersion()
+        ];
+
+        if (Craft::$app->request->headers->has('X-Inertia')) {
+            return $params;
+        }
+
+        return Craft::$app->view->renderTemplate(Inertia::getInstance()->view, [
+            'page' => $params
+        ]);
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     */
+    private function getInertiaProps($params = []): array
+    {
+        return array_merge(
+            Inertia::getInstance()->getShared(),
+            $params
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function getInertiaUrl(): string
+    {
+        return Craft::$app->request->getUrl();
+    }
+
+    /**
+     * @return string
+     */
+    private function getInertiaVersion(): string
+    {
+        return Inertia::getInstance()->getVersion();
+    }
+
+    public function checkOnly($key) {
+        return in_array($key, explode(',', $this->only), true);
+    }
+
+    public function getOnly(){
+        return $this->only;
+    }
+}
